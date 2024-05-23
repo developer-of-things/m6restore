cf915ee4509099e77f14255d75789ff495c3c01f
carlosalaniz <carlosglvn93@gmail.com>
2024-05-22 22:46:30 -0500

    a

--- m6restore.py
+++ m6restore.py
@@ -1,7 +1,3 @@
-HOST = "192.168.1.1"
-PORT = 5510
-TIMEOUT = 3
-
 import telnetlib
 import time
 import re
@@ -9,7 +5,11 @@ import sierrakeygen2
 import luhn
 import sys
 
-#verify supplied imei is valid
+HOST = "192.168.1.1"
+PORT = 5510
+TIMEOUT = 3
+
+# Verify supplied IMEI is valid
 newimei = sys.argv[1]
 if len(sys.argv) > 2 and re.match(r'^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$', sys.argv[2]):
     HOST = sys.argv[2]
@@ -17,73 +17,78 @@ if len(sys.argv) > 2 and re.match(r'^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$', sys.a
 imeiverification = luhn.verify(newimei)
 
 if imeiverification is False:
-    print ('Not a valid IMEI Number!')
+    print('Not a valid IMEI Number!')
     sys.exit()
-    
-#connect to device
+
+# Connect to device
 tn = telnetlib.Telnet(HOST, PORT, TIMEOUT)
 
-#grab some information about the device
-tn.write(b"ATI\r\n")
-time.sleep(10)
-atiresponse = tn.read_very_eager().decode('utf-8')
+# Function to read and print the response after a command
+def read_response():
+    time.sleep(10)
+    response = tn.read_very_eager().decode('utf-8')
+    print(response)
+    return response
 
-#grab the starting imei
-imeipattern = "\r\nIMEI: (.*?)\\r\nIMEI SV:"
+# Grab some information about the device
+tn.write(b"ATI\r\n")
+atiresponse = read_response()
+print("ATI Response: " + atiresponse)
+
+# Grab the starting IMEI
+imeipattern = r"\r\nIMEI: (.*?)\r\nIMEI SV:"
 imei = re.search(imeipattern, atiresponse).group(1)
 print("Device Current IMEI: " + imei)
 
-#grab the openlock hash
+# Grab the openlock hash
 tn.write(b"AT!OPENLOCK?\r\n")
-time.sleep(10)
-openlockresponse = tn.read_very_eager().decode('utf-8')
-#DEBUG: print("Open lock response: " + openlockresponse)
+openlockresponse = read_response()
+print("Open Lock Response: " + openlockresponse)
 
-#parse the response to a challenge
+# Parse the response to a challenge
 challenge = openlockresponse[15:-8]
 print("Open Lock Challenge: " + challenge)
 
-#keygen openlock hash
+# Keygen openlock hash
 keygen = sierrakeygen2.SierraGenerator()
-devicegeneration = "SDX55"
+devicegeneration = "SDX65"
 resp = keygen.run(devicegeneration, challenge, 0)
 print("Key Generator Response: " + resp)
 
-#throw that openlock hash over
+# Send the openlock hash
 print("Disabling Open Lock")
 openlockwrite = "AT!OPENLOCK=\"" + resp + "\"\r\n"
 tn.write(openlockwrite.encode('ascii'))
-time.sleep(10)
+read_response()
 
-#unlock imei
+# Unlock IMEI
 print("Unlocking IMEI")
 tn.write(b"AT!NVIMEIUNLOCK\r\n")
-time.sleep(10)
+read_response()
 
-#parse new imei with checksum
+# Parse new IMEI with checksum
 print("Updating IMEI")
 fullimei = luhn.append(newimei)
 encryptimei = ','.join(fullimei[i:i + 2] for i in range(0, len(fullimei), 2))
 encryptimeiwrite = "AT!NVENCRYPTIMEI=" + encryptimei + "\r\n"
 tn.write(encryptimeiwrite.encode('ascii'))
-time.sleep(10)
+read_response()
 
 print("Issuing AT!TELEN=1")
 tn.write(b"AT!TELEN=1\r\n")
-time.sleep(10)
+read_response()
 
 print("Issuing AT!CUSTOM=\"RDENABLE\",1")
-tn.write(b"AT!CUSTOM=\"RDENABLE\", 1\r\n")
-time.sleep(10)
+tn.write(b"AT!CUSTOM=\"RDENABLE\",1\r\n")
+read_response()
 
 print("Issuing AT!CUSTOM=\"TELNETENABLE\",1")
-tn.write(b"AT!CUSTOM=\"TELNETENABLE\", 1\r\n")
-time.sleep(10)
+tn.write(b"AT!CUSTOM=\"TELNETENABLE\",1\r\n")
+read_response()
 
-#reset router
+# Reset router
 print("IMEI Restored. Rebooting Router")
 tn.write(b"AT!RESET\r\n")
-tn.close()
+read_response()
 
-#DEBUG: routerresponse = tn.read_very_eager().decode('utf-8')
-#DEBUG: print("Router Response: " + routerresponse)
+tn.close()
